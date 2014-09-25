db65a5d96b59bb7abe38106477308a5cdb7bebda
hepeace <heningok@126.com>
2014-09-25 19:45:15 +0800

    upload

---
+++ if.py
@@ -0,0 +1,8 @@
+#!/usr/local/bin/python
+#
+x = input("Please Input First Number:")
+y = input("Please Input Second Number:")
+if x < y :
+	print '%d is less than %d!' %(x,y)
+else:
+	print '%d is geater than %d!' %(x,y)
---
+++ while.py
@@ -0,0 +1,7 @@
+#!/usr/local/bin/python
+#
+num = input("Please Input A Number:")
+while num <= 5 :
+	print 'The Num Is %d!' %(num)
+	num+=1
+
